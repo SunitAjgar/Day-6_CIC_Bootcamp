e1dadc0f74a4202bfcf74e45efee1070ef6021ac
Sunit Ajgar <109612599+Sunit47@users.noreply.github.com>
2022-08-02 13:17:13 +0530

    Add files via upload

---
+++ Day 6 for loop/Q4.sh
@@ -0,0 +1,19 @@
+echo Range is between minimum and maximum number
+echo enter min number and max number
+read min max
+for a in $(seq $min $max)
+do
+    k=0
+    for i in $(seq 2 $(expr $a - 1))
+    do 
+        if [ $(expr $a % $i) -eq 0 ]
+        then
+            k=1
+            break
+        fi
+    done
+    if [ $k -eq 0 ]
+    then
+    echo $a
+    fi
+done
